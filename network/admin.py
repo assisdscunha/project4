61c7907dcb05f44a7cba37e56a112ed038307046
assisdacunha <samuelhm@gmail.com>
2025-04-20 09:05:15 -0300

    added models to admin page

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from network.models import Like, Posts, User
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Posts)
+admin.site.register(Like)
